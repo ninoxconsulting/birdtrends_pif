1d741308fb1dd5879495c7bdb3e0658d96983b1f
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2024-03-28 17:10:32 -0700

    add testing trend and plot functions

--- 04.pif_workflow.R
+++ 04.pif_workflow.R
@@ -23,6 +23,11 @@ inds <- generate_indices(fit,regions = "country")
 indsmooth <- generate_indices(fit,regions = "country",
                               alternate_n = "n_smooth")
 
+plot_indices(inds)
+
+ttemp <- generate_trends(indsmooth)
+aa <- ttemp$trends
+
 
 # Input option 1 - annual indices of relative abundance with CI estimates
 # data-frame with at a minimum, columns
@@ -171,133 +176,358 @@ comp_plot <- ggplot2::ggplot(data = sel_hgams,
   
   
   
+  
+  
+  ######################################################################
   # calculate trend 
   
   # convert data to long form? 
   
-  ldf <- tibble::rowid_to_column(fitted_smooths, "draw") %>%
+  ldf_smooths <- tibble::rowid_to_column(fitted_smooths, "draw") %>%
     tidyr::pivot_longer(., cols = !starts_with("d")) %>%
     dplyr::rename('year' = name, "proj_y" = value)%>%
     mutate(year = as.integer(year))
   
   
-  trend <- function(proj_data, 
-                    start_yr = 2014, 
-                    end_yr = 2022, 
-                    proj_yr = 2046,
-                    method = "geomean"){
-    
-    # testing 
-    proj_data <- ldf 
-    start_yr = 2014
-    end_yr = 2022
-    proj_yr = 2046
-    
+ # ldf <- nest(fitted_smooths)
+
+  ldf_gams <- tibble::rowid_to_column(fitted_gams, "draw") %>%
+    tidyr::pivot_longer(., cols = !starts_with("d")) %>%
+    dplyr::rename('year' = name, "proj_y" = value)%>%
+    mutate(year = as.integer(year))
+  
+  
+  
+  
+  
+
+  
+  predict_trend <- function(proj_data, 
+                    start_yr = NA, 
+                    end_yr = NA, 
+                    proj_yr = 2046){
+                    #method = "geomean"
+                  
+  #   # testing 
+  # proj_data <- ldf
+  #  start_yr = NA
+  #  end_yr = 2022
+  #  proj_yr = 2046
+
     
     if(is.na(start_yr)){start_yr = min(proj_data$year)}
     if(is.na(end_yr)){end_yr = max(proj_data$year)}
   
+    #proj_start_yr = end_yr + 1
+    #proj_seq <- seq(proj_start_yr, proj_yr)
     
     trend_dat <- subset(proj_data, year %in% seq(start_yr, end_yr))
+  
     
-    pred_dat <- proj_data  
-    
-    
-    
-    #if(method == "geomean") {
-      
-      #i_dat_for_trend <- subset(i_dat, Year %in% seq(year_goals_are_set,year_goals_are_set - length_current_trend))
-      #trend_log <- mean(diff(i_dat_for_trend$gam_pred))
-      #trend_percent <- 100*(exp(trend_log)-1) # Convert to percent change per year
+   # if(method == "geomean") {
       
       
       # estimate the trend based on the years of selection for each draw
       
       trend_sum <- trend_dat %>%
         group_by(draw) %>%
-        summarise(trend_log = mean(diff(log(proj_y))))# %>%
-        #mutate(perc_trend = 100*(exp(trend_log)-1))
+        summarise(trend_log = mean(diff(log(proj_y)))) %>%
+        mutate(perc_trend = 100*(exp(trend_log)-1))
+
+      
+    #  }  else if (method == "lm"){
     
+    # method for lm 
+    
+    #trendlls_log <- lm(gam_pred ~ Year, data = i_dat)
+    #trendlls_log <- as.numeric(trendlls_log$coefficients[2])
+    #trendlls_percent <- 100*(exp(trendlls_log)-1)
+    
+   #   }
+  #unnest()
+      pred_out <- proj_data[1,]%>% mutate(pred_ind = 0)
       
-      preds <- foreach::foreach(i = names(out), .combine = rbind)%do%{
-        # testing line
-        #i = names(out)[1]
-        
-        i_dat <- data.frame(log_y = as.vector(unlist(out[i])), Year = year_seq)
-        
-        gam <- gam(log_y~s(Year, k = n_knots, bs = "tp"),
-                   data = i_dat) ## or
-        
-        #gam <- gam(log_y~s(year, bs = 'cs', k = length(knots)),
-        #           knots = list(Year = knots),
-        #           data = i_dat)
-        
-        
-        i_dat$gam_pred <- stats::predict(gam, newdata = i_dat)
-        predvals <- exp(i_dat$gam_pred)
-        
-     
-        
-      }
       
-  
       for( i in trend_sum$draw){
         
-         i  = trend_sum$draw[1]
+        print(i)
+        
+         #i  = trend_sum$draw[2]
         
         trend_draw <- trend_sum %>% filter(draw == i) %>% pull(trend_log)
         
         proj_draw <- proj_data %>% filter(draw == i) 
         
-        proj_draw <-  proj_draw %>%
-           complete(year = seq(min(proj_data$year), max(proj_yr), 1)) %>%
+        
+        # not sure where to cut of projections here: 
+        #ie build trend from project from 2014 - 2022
+        # project to 2024 
+        # project to 2046
+        
+        proj_draw <- proj_draw %>%
+           complete(year = seq(end_yr, max(proj_yr), 1)) %>%
            mutate(draw = i) %>%
-           mutate(pred_ind = proj_y) 
-        
-      #   
-      # # historic trend
-      # for (y in seq((min(proj_draw$year)+1),start_yr)){
-      #   #y = 1969
-      #     proj_draw$pred_ind[proj_draw$year == y] <- proj_draw$pred_ind[proj_draw$year == (y-1)] *exp(trend_draw)
-      #      }
-      
-        
-        
+           #filter(year >= proj_start_yr)
+           mutate(pred_ind = proj_y)%>%
+           arrange(year)
         
       #  calculate future projections starting in year of goal
 
-      for (y in seq(end_yr+1,proj_yr)) {
+      for (y in seq(end_yr + 1, proj_yr)) {
         proj_draw$pred_ind[proj_draw$year == y] <- proj_draw$pred_ind[proj_draw$year == (y-1)] *exp(trend_draw)
       }
-       
-      #
+  
+        pred_out <- bind_rows(pred_out, proj_draw)
+      #return(proj_draw)
     
-    
-      trend_sum <- trend_sum %>% filter(draw == 1)
-      
-      
-      
-      
-    
-    
-        } ifelse(method == "lm"){
-      
-      # method for lm 
-      
-      #trendlls_log <- lm(gam_pred ~ Year, data = i_dat)
-      #trendlls_log <- as.numeric(trendlls_log$coefficients[2])
-      #trendlls_percent <- 100*(exp(trendlls_log)-1)
-      
       }
+      pred_out <- pred_out[-1,]
+      
+      return(pred_out)
     
-    return(trend_sum)
-    
-    }
+    } # end of function
   
  
   
   
   
+  preds_sm <- predict_trend(ldf_smooths, start_yr = 2014, end_yr = 2022, proj_yr = 2050)
+  
+  preds_gams <- predict_trend(ldf_gams, start_yr = 2014, end_yr = 2022, proj_yr = 2050)
   
   
+  ## summary of indices 
+  
+  
+  # ----------------------------------------------------
+  # Summarize indices each year
+  # ----------------------------------------------------
+
+  trend_plot <- function(raw_indices = input_option_1, 
+                         model_indices = ldf, 
+                         pred_indices = pred_out,
+                         start_yr = 2014, 
+                         end_yr = 2022){
+  
+  indices_summarized <- raw_indices #%>%
+  #   tibble::rowid_to_column(., "draw") %>%
+  #   tidyr::pivot_longer(., cols = !starts_with("d")) %>%
+  #   dplyr::rename('year' = name, "obs_y" = value)%>%
+  #   mutate(year = as.integer(year)) %>%
+  #   group_by(year) %>%
+  #   summarize(Index_q_0.025 = quantile(obs_y,0.025),
+  #             Index = quantile(obs_y,0.5),
+  #             Index_q_0.975 = quantile(obs_y,0.975)) 
+  # 
+  
+  gam_summarized <- model_indices %>%
+    group_by(year) %>%
+    summarize(gam_q_0.025 = quantile(proj_y,0.025),
+              gam_index = quantile(proj_y,0.5),
+              gam_q_0.975 = quantile(proj_y,0.975)) 
+  
+  predict_summarized <- pred_indices |> 
+    group_by(year)%>%
+    summarize(pred_q_0.025 = quantile(pred_ind,0.025),
+              pred_index = quantile(pred_ind,0.5),
+              pred_q_0.975 = quantile(pred_ind,0.975)) %>% 
+    filter(year >= end_yr)
+    
+  
+  Index_baseline <- pred_indices %>% 
+    filter(year == 2014)
+  
+  ref_year = 2014
+  
+  
+  baseline_years <- gam_summarized %>%
+    filter(year %in% c(start_yr, end_yr))
+  
+  
+  sp_plot_index <- ggplot() +
+    
+    # Vertical line showing the year goals were set
+    geom_vline(xintercept =  ref_year, size=2, col = "black", alpha = 0.2)+
+    #geom_text(aes(x =  ref_year+1, y = 0.01), 
+    #          label = "<- reference year", col = "black", alpha = 0.2,
+    #          hjust=0, fontface = "bold", size = 2)+
+    
+    geom_vline(xintercept = 2024, size=1, col = "black", alpha = 0.4,  linetype="dotted")+
+    geom_vline(xintercept = 2046, size=1, col = "black", alpha = 0.4, linetype="dotted")+
+    
+    # Observed indices
+    geom_errorbar(data = subset(indices_summarized, year <= max(indices_summarized$year)),aes(x = year, ymin = index_q_0.025, ymax = index_q_0.975), width = 0, col = "gray30")+
+    geom_point(data = subset(indices_summarized, year <= max(indices_summarized$year)),aes(x = year, y = index), col = "gray30")+
+    
+  # gam smooth
+    geom_ribbon(data = gam_summarized, aes(x = year, ymin = gam_q_0.025, ymax = gam_q_0.975), alpha = 0.4, fill = "gray50")+
+    geom_line(data = gam_summarized, aes(x = year, y = gam_index), col = "gray50", linewidth = 1)+
+    
+  # predicted projection
+    geom_ribbon(data = subset(predict_summarized, year >= end_yr), aes(x = year, ymin = pred_q_0.025, ymax = pred_q_0.975), alpha = 0.2, fill = "orangered")+
+    geom_line(data = subset(predict_summarized, year >= end_yr), aes(x = year, y = pred_index), col = "orangered", linewidth = 1)+
+  
+  
+    #geom_line(data = baseline_years, aes(x = year, y = gam_index), col = "black", linewidth = 1)+
+    
+    ylab("Population Index")+
+    xlab("Year")+
+    theme_bw()
+    #coord_cartesian(ylim=c(0,max(apply(sp_projection$indices,2,function(x) quantile(x, 0.975)))),
+     #               xlim=c(1970,2050))+
+    #scale_x_continuous(breaks = seq(1970,sp_projection$end_of_projection,10))
+  
+  print(sp_plot_index)
+  
+  } 
+
+
+  
+  smooth_plot <- trend_plot(raw_indices = input_option_1, 
+             model_indices = ldf_smooths, 
+             pred_indices = preds_sm,
+             start_yr = 2014, 
+             end_yr = 2022)
+  
+  gam_plot <- trend_plot(raw_indices = input_option_1, 
+                         model_indices = ldf_gams, 
+                         pred_indices =  preds_gams,
+                         start_yr = 2014, 
+                         end_yr = 2022)
+  
+  
+                         
+ 
+  
+  
+ #  
+ # 
+ #  
+ #  percent_of_Baseline_StatusQuo <- indices_StatusQuo
+ #  percent_of_Baseline_Recovery <- indices_Recovery
+ #  
+ #  for (j in 1:ncol(percent_of_Baseline_StatusQuo)){
+ #    percent_of_Baseline_StatusQuo[,j] <- 100* percent_of_Baseline_StatusQuo[,j]/Index_baseline
+ #    percent_of_Baseline_Recovery[,j] <- 100* percent_of_Baseline_Recovery[,j]/Index_baseline
+ #  }
+ #  
+ #  percent_of_Baseline_Obs <- indicies
+ #  for (j in 1:ncol(percent_of_Baseline_Obs)) percent_of_Baseline_Obs[,j] <- 100* percent_of_Baseline_Obs[,j]/Index_baseline
+ #  
+ #  
+ #  
+ #  
+ #  
+ #  
+ #  
+ #  
+ #  
+ #  
+ #  
+ #  
+ #  
+ #  
+ #  
+ #  
+ #  
+ #  
+ #  
+ #  indices <- input_option_3
+ #  min_year = NULL
+ #  max_year = NULL
+ #  quantiles = c(0.025, 0.05, 0.25, 0.75, 0.95, 0.975)
+ #  #slope = FALSE
+ #  #prob_decrease = NULL
+ #  #prob_increase = NULL
+ # # hpdi = FALSE
+ #  calc_quantiles <- stats::quantile
+ #  
+ #  
+ #  # generate trends 
+ #  yr_seq <- seq(min(as.integer(colnames(indices))), max(as.integer(colnames(indices))))
+ #  
+ #  start_year <- min(as.integer(colnames(indices)))
+ #  n_years <- length(yr_seq)
+ #  indx <- indices
+ #  
+ #  
+ #  if(is.null(min_year)) {
+ #    min_year <- start_year
+ #  } else {
+ #    
+ #    if(min_year < start_year) {
+ #      message("`min_year` is before the date range, using minimum year of ",
+ #              "the data (", min_year <- start_year, ") instead.")
+ #    }
+ #  }
+ #  
+ #  if (is.null(max_year)) {
+ #    max_year <- max(as.integer(colnames(indices)))
+ #  } else if(max_year > max(as.integer(colnames(indices)))) {
+ #    message("`max_year` is beyond the date range, using maximum year of ",
+ #            "the data (", max_year <- max(as.integer(colnames(indices))), ") instead.")
+ #  }
+ #  
+ #  
+ #  # For indexing
+ #  min_year_num <- min_year - start_year + 1
+ #  max_year_num <- max_year - start_year + 1
+ #  
+ #  
+ #  library(purrr)
+ #  
+ #  
+ #  
+ #  
+ #  trends <- indices %>%
+ #    #dplyr::filter(.data$year %in% min_year:max_year) %>%
+ #    #dplyr::group_by(.data$region, .data$region_type,
+ #    #                .data$strata_included, .data$strata_excluded) %>%
+ #    dplyr::summarize(
+ #      # Basic statistics
+ #      #rel_abundance = mean(.data$index),
+ #      #obs_rel_abundance = mean(.data$obs_mean),
+ #      #mean_n_routes = mean(.data$n_routes),
+ #      #n_routes = mean(.data$n_routes_total),
+ #      #backcast_flag = mean(.data$backcast_flag),
+ #      
+ #      # Metadata
+ #      start_year = .env$min_year,
+ #      end_year = .env$max_year) %>%
+ #    
+ #    dplyr::mutate(
+ #      #n_strata_included = purrr::map_dbl(
+ #      #  .data$strata_included, ~length(unlist(stringr::str_split(.x, " ; ")))),
+ #      
+ #      # Add in samples
+ #      n = purrr::map2(.data,
+ #                      ~indices[[paste0(.x, "_", .y)]]))#,
+ #      # Calculate change start to end for each iteration
+ #      ch = purrr::map(.data$n,
+ #                      ~.x[, .env$max_year_num] / .x[, .env$min_year_num]),
+ #      # Calculate change as trend for each iteration
+ #      tr = purrr::map(
+ #        .data$ch,
+ #        ~100 * ((.x^(1/(.env$max_year_num - .env$min_year_num))) - 1)),
+ #      
+ #      # Median and percentiles of trend per region
+ #      trend = purrr::map_dbl(.data$tr, stats::median),
+ #      trend_q = purrr::map_df(
+ #        .data$tr,
+ #        ~stats::setNames(calc_quantiles(.x, quantiles, names = FALSE),
+ #                         paste0("trend_q_", quantiles))),
+ #      
+ #      # Percent change and quantiles thereof per region
+ #      percent_change = purrr::map_dbl(.data$ch, ~100 * (stats::median(.x) - 1)),
+ #      pc_q = purrr::map_df(
+ #        .data$ch, ~stats::setNames(
+ #          100 * (calc_quantiles(.x, quantiles, names = FALSE) - 1),
+ #          paste0("percent_change_q_", quantiles)))) %>%
+ #    dplyr::ungroup() %>%
+ #    tidyr::unnest(cols = c("trend_q", "pc_q")) %>%
+ #    dplyr::arrange(.data$region_type, .data$region)
+ #  
+ #  
+ #  
+ #  
 
