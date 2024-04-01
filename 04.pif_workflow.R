1ccbf4739c7eb76282654fb655ff9b061c1e5094
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2024-04-01 10:33:36 -0700

    update plot step using birdtrend

--- 04.pif_workflow.R
+++ 04.pif_workflow.R
@@ -1,6 +1,6 @@
 # PIF workflow using birdtrends
 
-devtools::install_github("ninoxconsulting/birdtrends")
+#devtools::install_github("ninoxconsulting/birdtrends")
 library(mgcv)
 library(tidyverse)
 library(birdtrends)
@@ -226,111 +226,111 @@ preds_gams <- predict_trend(ldf_gams, trend_gam,start_yr = 2023, proj_yr = 2050)
   
 
 
-
-## UP TO HERE 
-
-
-  # ----------------------------------------------------
-  # Summarize indices each year
-  # ----------------------------------------------------
-
-  trend_plot <- function(raw_indices = indat1, 
-                         model_indices = ldf, 
-                         pred_indices =  preds_sm ,
-                         start_yr = 2014, 
-                         end_yr = 2022){
-  
-  indices_summarized <- raw_indices #%>%
-  #   tibble::rowid_to_column(., "draw") %>%
-  #   tidyr::pivot_longer(., cols = !starts_with("d")) %>%
-  #   dplyr::rename('year' = name, "obs_y" = value)%>%
-  #   mutate(year = as.integer(year)) %>%
-  #   group_by(year) %>%
-  #   summarize(Index_q_0.025 = quantile(obs_y,0.025),
-  #             Index = quantile(obs_y,0.5),
-  #             Index_q_0.975 = quantile(obs_y,0.975)) 
-  # 
-  
-  gam_summarized <- model_indices %>%
-    group_by(year) %>%
-    summarize(gam_q_0.025 = quantile(proj_y,0.025),
-              gam_index = quantile(proj_y,0.5),
-              gam_q_0.975 = quantile(proj_y,0.975)) 
-  
-  predict_summarized <- pred_indices |> 
-    group_by(year)%>%
-    summarize(pred_q_0.025 = quantile(pred_ind,0.025),
-              pred_index = quantile(pred_ind,0.5),
-              pred_q_0.975 = quantile(pred_ind,0.975)) %>% 
-    filter(year >= end_yr)
-    
-  
-  Index_baseline <- pred_indices %>% 
-    filter(year == 2014)
-  
-  ref_year = 2014
-  
-  
-  baseline_years <- gam_summarized %>%
-    filter(year %in% c(start_yr, end_yr))
-  
-  
-  sp_plot_index <- ggplot() +
-    
-    # Vertical line showing the year goals were set
-    geom_vline(xintercept =  ref_year, size=2, col = "black", alpha = 0.2)+
-    #geom_text(aes(x =  ref_year+1, y = 0.01), 
-    #          label = "<- reference year", col = "black", alpha = 0.2,
-    #          hjust=0, fontface = "bold", size = 2)+
-    
-    geom_vline(xintercept = 2024, size=1, col = "black", alpha = 0.4,  linetype="dotted")+
-    geom_vline(xintercept = 2046, size=1, col = "black", alpha = 0.4, linetype="dotted")+
-    
-    # Observed indices
-    geom_errorbar(data = subset(indices_summarized, year <= max(indices_summarized$year)),aes(x = year, ymin = index_q_0.025, ymax = index_q_0.975), width = 0, col = "gray30")+
-    geom_point(data = subset(indices_summarized, year <= max(indices_summarized$year)),aes(x = year, y = index), col = "gray30")+
-    
-  # gam smooth
-    geom_ribbon(data = gam_summarized, aes(x = year, ymin = gam_q_0.025, ymax = gam_q_0.975), alpha = 0.4, fill = "gray50")+
-    geom_line(data = gam_summarized, aes(x = year, y = gam_index), col = "gray50", linewidth = 1)+
-    
-  # predicted projection
-    geom_ribbon(data = subset(predict_summarized, year >= end_yr), aes(x = year, ymin = pred_q_0.025, ymax = pred_q_0.975), alpha = 0.2, fill = "orangered")+
-    geom_line(data = subset(predict_summarized, year >= end_yr), aes(x = year, y = pred_index), col = "orangered", linewidth = 1)+
-  
-  
-    #geom_line(data = baseline_years, aes(x = year, y = gam_index), col = "black", linewidth = 1)+
-    
-    ylab("Population Index")+
-    xlab("Year")+
-    theme_bw()
-    #coord_cartesian(ylim=c(0,max(apply(sp_projection$indices,2,function(x) quantile(x, 0.975)))),
-     #               xlim=c(1970,2050))+
-    #scale_x_continuous(breaks = seq(1970,sp_projection$end_of_projection,10))
-  
-  print(sp_plot_index)
-  
-  } 
+######################################################################
+# 5. plot graphs 
+##########################################################
 
 
-  
-  smooth_plot <- trend_plot(raw_indices = input_option_1, 
-             model_indices = ldf_smooths, 
-             pred_indices = preds_sm,
-             start_yr = 2014, 
-             end_yr = 2022)
-  
-  gam_plot <- trend_plot(raw_indices = input_option_1, 
-                         model_indices = ldf_gams, 
-                         pred_indices =  preds_gams,
+hgams_plot <- trend_plot(raw_indices = input_option_1, 
+                         model_indices = ldf_hgam, 
+                         pred_indices = preds_hgam,
                          start_yr = 2014, 
                          end_yr = 2022)
-  
-  
-                         
- 
-  
-  
+
+smooth_plot <- trend_plot(raw_indices = input_option_1, 
+                          model_indices = ldf_smooths, 
+                          pred_indices = preds_sm,
+                          start_yr = 2014, 
+                          end_yr = 2022)
+
+gam_plot <- trend_plot(raw_indices = input_option_1, 
+                       model_indices = ldf_gams, 
+                       pred_indices =  preds_gams,
+                       start_yr = 2014, 
+                       end_yr = 2022)
+
+# 
+#   # ----------------------------------------------------
+#   # Summarize indices each year
+#   # ----------------------------------------------------
+# 
+#   trend_plot <- function(raw_indices = indat1, 
+#                          model_indices = ldf_hgam, 
+#                          pred_indices =  preds_hgam ,
+#                          start_yr = 2014, 
+#                          end_yr = 2022){
+#   
+#   indices_summarized <- raw_indices #%>%
+#   #   tibble::rowid_to_column(., "draw") %>%
+#   #   tidyr::pivot_longer(., cols = !starts_with("d")) %>%
+#   #   dplyr::rename('year' = name, "obs_y" = value)%>%
+#   #   mutate(year = as.integer(year)) %>%
+#   #   group_by(year) %>%
+#   #   summarize(Index_q_0.025 = quantile(obs_y,0.025),
+#   #             Index = quantile(obs_y,0.5),
+#   #             Index_q_0.975 = quantile(obs_y,0.975)) 
+#   # 
+#   
+#   gam_summarized <- model_indices %>%
+#     group_by(year) %>%
+#     summarize(gam_q_0.025 = quantile(proj_y,0.025),
+#               gam_index = quantile(proj_y,0.5),
+#               gam_q_0.975 = quantile(proj_y,0.975)) 
+#   
+#   predict_summarized <- pred_indices |> 
+#     group_by(year)%>%
+#     summarize(pred_q_0.025 = quantile(pred_ind,0.025),
+#               pred_index = quantile(pred_ind,0.5),
+#               pred_q_0.975 = quantile(pred_ind,0.975)) %>% 
+#     filter(year >= end_yr)
+#     
+#   #Index_baseline <- pred_indices %>% 
+#   #  filter(year == 2014)
+#   
+#   ref_year = 2014
+#   
+#   
+#   baseline_years <- gam_summarized %>%
+#     filter(year %in% c(start_yr, end_yr))
+#   
+#   
+#   sp_plot_index <- ggplot() +
+#     
+#     # Vertical line showing the year goals were set
+#     geom_vline(xintercept =  ref_year, size=2, col = "black", alpha = 0.2)+
+#     #geom_text(aes(x =  ref_year+1, y = 0.01), 
+#     #          label = "<- reference year", col = "black", alpha = 0.2,
+#     #          hjust=0, fontface = "bold", size = 2)+
+#     
+#     geom_vline(xintercept = 2024, size=1, col = "black", alpha = 0.4,  linetype="dotted")+
+#     geom_vline(xintercept = 2046, size=1, col = "black", alpha = 0.4, linetype="dotted")+
+#     
+#     # Observed indices
+#     geom_errorbar(data = subset(indices_summarized, year <= max(indices_summarized$year)),aes(x = year, ymin = index_q_0.025, ymax = index_q_0.975), width = 0, col = "gray30")+
+#     geom_point(data = subset(indices_summarized, year <= max(indices_summarized$year)),aes(x = year, y = index), col = "gray30")+
+#     
+#   # gam smooth
+#     geom_ribbon(data = gam_summarized, aes(x = year, ymin = gam_q_0.025, ymax = gam_q_0.975), alpha = 0.4, fill = "gray50")+
+#     geom_line(data = gam_summarized, aes(x = year, y = gam_index), col = "gray50", linewidth = 1)+
+#     
+#   # predicted projection
+#     geom_ribbon(data = subset(predict_summarized, year >= end_yr), aes(x = year, ymin = pred_q_0.025, ymax = pred_q_0.975), alpha = 0.2, fill = "orangered")+
+#     geom_line(data = subset(predict_summarized, year >= end_yr), aes(x = year, y = pred_index), col = "orangered", linewidth = 1)+
+#   
+#   
+#     #geom_line(data = baseline_years, aes(x = year, y = gam_index), col = "black", linewidth = 1)+
+#     
+#     ylab("Annual Index of Abundance")+
+#     xlab("Year")+
+#     theme_bw()
+#     #coord_cartesian(ylim=c(0,max(apply(sp_projection$indices,2,function(x) quantile(x, 0.975)))),
+#      #               xlim=c(1970,2050))+
+#     #scale_x_continuous(breaks = seq(1970,sp_projection$end_of_projection,10))
+#   
+#   print(sp_plot_index)
+#   
+#   } 
+# 
  #  
  # 
  #  
@@ -347,21 +347,7 @@ preds_gams <- predict_trend(ldf_gams, trend_gam,start_yr = 2023, proj_yr = 2050)
  #  
  #  
  #  
- #  
- #  
- #  
- #  
- #  
- #  
- #  
- #  
- #  
- #  
- #  
- #  
- #  
- #  
- #  
+ 
  #  
  #  indices <- input_option_3
  #  min_year = NULL
